4e04149bb63bb29633305c0cdad65ecad934c628
MuhammadFauzi <92508010+oneup1945@users.noreply.github.com>
2022-03-19 22:49:16 +0700

    Update Kalkulator.cpp

--- Kalkulator.cpp
+++ Kalkulator.cpp
@@ -1,1 +1,35 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    int i,j,hasil;
+    char k;
+    
+    
+    cout << "Kalkulator"<<endl;
+    cout << "masukkan nilai pertama:"<<endl;cin>>i;
+    cout << " masukan nilai kedua:"<<endl; cin>>j;
+    cout << " pilihan pengoperasian= +,-,*,/";cin>>k;
+    
+    cout << " k :";
+    cout << i << " "<<hasil<<" "<< j <<endl;
+    switch(k){
+       case '+':
+            hasil= i + j;
+            break;
+       case '-':
+            hasil= i - j;
+            break;
+       case '*':
+            hasil= i * j;
+            break;  
+        case ':':
+            hasil= i / j;
+            break;       
+    }
+       cout <<i<<k<<j<<"="<<hasil<<endl;
+    
+       cin.get();
+       return 0;
+}
